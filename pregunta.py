3c716cec050b90cb6ad344315e0a9a1408675ef7
Alejandra <112440494+apabond@users.noreply.github.com>
2022-11-23 20:39:34 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,29 +15,30 @@ import pandas as pd
 def ingest_data():
     import re
 
-    i = 0
-    dict_linea = {}
-    df = pd.DataFrame()
-    with open('./clusters_report.txt') as f:
-        for line in f:
-
+    contador = 0
+    dictionary = {}
+    dataframe = pd.DataFrame()
+    
+    with open('./clusters_report.txt') as data:
+        for line in data:
             line = re.sub(r"\s+", " ", line)
-            if len(line)>1 and i > 3:
+            if len(line)>1 and contador > 3:
                 if line.split()[0].isnumeric() == True:
                     try: 
-                        dict_linea['principales_palabras_clave'] = ' '.join(dict_linea['principales_palabras_clave'])
-                        df = df.append(dict_linea, ignore_index=True)
+                        dictionary['principales_palabras_clave'] = ' '.join(dictionary['principales_palabras_clave'])
+                        dataframe = dataframe.append(dictionary, ignore_index=True)
                     except: pass
-                    dict_linea = {'cluster': int(line.split()[0]),
+                    dictionary = {'cluster': int(line.split()[0]),
                                 'cantidad_de_palabras_clave': int(line.split()[1]),
                                 'porcentaje_de_palabras_clave': float(line.split()[2].replace(',','.')),
                                 'principales_palabras_clave': line.split()[4:]}
                 else: 
-                    dict_linea['principales_palabras_clave'].append(' '.join(line.split()))
-            i += 1
-    dict_linea['principales_palabras_clave'] = ' '.join(dict_linea['principales_palabras_clave'])
-    df = df.append(dict_linea, ignore_index=True)
-    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.rstrip('\.')
+                    dictionary['principales_palabras_clave'].append(' '.join(line.split()))
+                    
+            contador += 1
+            
+    dictionary['principales_palabras_clave'] = ' '.join(dictionary['principales_palabras_clave'])
+    dataframe = dataframe.append(dictionary, ignore_index=True)
+    dataframe['principales_palabras_clave'] = dataframe['principales_palabras_clave'].str.rstrip('\.')
     
-    
-    return df
+    return dataframe
