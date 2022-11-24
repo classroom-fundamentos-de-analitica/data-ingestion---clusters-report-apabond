09e44e110bbe4cbe5ba849c53ea2556b74c8b825
Alejandra <112440494+apabond@users.noreply.github.com>
2022-11-23 20:26:08 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,34 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
+    
+    dictionary = {}
+    contador = 0
+    df = pd.DataFrame()
+    
+    with open('./clusters_report.txt') as data:
+        for line in data:
+            line = re.sub(r"\s+", " ", line)
+            if len(line)>1 and contador > 3:
+                if line.split()[0].isnumeric() == True:
+                    try: 
+                        dictionary['principales_palabras_clave'] = ' '.join(dictionary['principales_palabras_clave'])
+                        df = df.append(dictionary, ignore_index=True)
+                    except: pass
+                    dictionarya = {'cluster': int(line.split()[0]),
+                                'cantidad_de_palabras_clave': int(line.split()[1]),
+                                'porcentaje_de_palabras_clave': float(line.split()[2].replace(',','.')),
+                                'principales_palabras_clave': line.split()[4:]}
+                else: 
+                    dictonary['principales_palabras_clave'].append(' '.join(line.split()))
+            contador += 1
+    dictionary['principales_palabras_clave'] = ' '.join(dictionary['principales_palabras_clave'])
+    df = df.append(dictionary, ignore_index=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.rstrip('\.')
+    
+    
 
     return df
